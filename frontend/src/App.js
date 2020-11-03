d47c4dad32d9174b9319311ec1ab8de803681fd9
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-11-03 13:20:43 +0200

    Add mock data. Create Product component and Home screen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
       <main className="py-3">
         <Container>
           <h1>Welcome to the App</h1>
+
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
