bec11c56bb74e86784ed1dfe763d50678c15e6df
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-11-04 16:20:42 +0200

    Add product list reducer and actions.

---
+++ frontend/src/reducer/productReducer.js
@@ -0,0 +1,34 @@
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+
+const initialState = {
+  products: [],
+  loading: false,
+  error: null
+};
+
+const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case PRODUCT_LIST_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+    case PRODUCT_LIST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        products: payload
+      };
+    case PRODUCT_LIST_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default productReducer;
