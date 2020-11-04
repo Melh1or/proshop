383e8c0520ad597014753af4f8b1374d393c9d90
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-11-04 13:17:12 +0200

    Add product route.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import colors from "colors";
+
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -10,20 +11,7 @@ connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (res, req) => {
-  const { id } = res.params;
-  const product = products.find(p => p._id === id);
-
-  req.json(product);
-});
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000;
 
